1b7c9c75f096efe90def24305ef365a8b6b0055b
luis phelipe <scphelipe@gmail.com>
2022-12-10 16:18:42 -0300

    initial commit

---
+++ 2022/01/01.py
@@ -0,0 +1,19 @@
+import sys
+
+
+def main():
+    total = []
+    current = 0
+
+    for line in sys.stdin:
+        if line == "\n":
+            total.append(current)
+            current = 0
+            continue
+
+        current += int(line)
+
+    print(max(total))
+
+
+main()
